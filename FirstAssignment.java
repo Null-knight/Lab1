676ba323ee9fb359407b90fa71c224d9ee804f3e
Null-knight <251553013@kiit.ac.in>
2025-12-07 18:33:37 +0530

    Update actual 1st assignment file

--- FirstAssignment.java
+++ FirstAssignment.java
@@ -1,5 +1,16 @@
+// 1.Write a program to print your name, roll no, section and branch in separate lines.
+
 public class FirstAssignment {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+
+        String name = "Rahul Roy";
+        String rollNo = "23CS1001";
+        String section = "A";
+        String branch = "Computer Science";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Section: " + section);
+        System.out.println("Branch: " + branch);
     }
 }
