1b28fdb25b6c22374ac61deeb2287497e390a1c9
Null-knight <251553013@kiit.ac.in>
2025-12-07 17:58:27 +0530

    Just testing in Vs code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 //java program to print hello world
-  public class HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
